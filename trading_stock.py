f5283cd1590acaeb436b45f96723412513c2763e
lukasmartinelli <me@lukasmartinelli.ch>
2016-05-11 16:36:53 +0200

    Improve min/max and negative stock courses

--- trading_stock.py
+++ trading_stock.py
@@ -1,19 +1,20 @@
 stock_prices_yesterday = [10, 7, 5, 8, 3, 4, 10, 11, 2, 4, 5]
+stock_prices_crash = [10, 7, 5, 1]
 
 
 def max_profit_yesterday(stock_prices):
-    max_profit = 0
+    max_profit = stock_prices[1] - stock_prices[0]
     min_price = stock_prices[0]
 
-    for price in stock_prices:
-        if price < min_price:
-            min_price = price
-
+    for price in stock_prices[1:]:
         possibe_profit = price - min_price
-        if possibe_profit > max_profit:
-            max_profit = possibe_profit
+        max_profit = max(possibe_profit, max_profit)
+        min_price = min(min_price, price)
 
     return max_profit
 
 
 print(max_profit_yesterday(stock_prices_yesterday))
+
+# Stock goes only down - profit is negative
+print(max_profit_yesterday(stock_prices_crash))
