a82bcd66883da4c37afa0e3b4273f537cc9707db
lukasmartinelli <me@lukasmartinelli.ch>
2016-06-10 19:11:13 +0200

    Solve the very simple stack calculator

--- challenges/evaluate_expression.py
+++ challenges/evaluate_expression.py
@@ -1,37 +1,3 @@
-import collections
-
-
-def reduce_notation(notation):
-    reduced_notation = collections.deque()
-
-    if len(notation) == 1:
-        return notation
-
-    while len(notation) > 2:
-        op = notation.pop()
-        right = notation.pop()
-        left = notation.pop()
-
-        if not is_operand(op):
-            reduced_notation.appendleft(op)
-            reduced_notation.appendleft(right)
-            reduced_notation.appendleft(left)
-        elif is_operand(left):
-            notation.append(left)
-            reduced_notation.appendleft(op)
-            reduced_notation.appendleft(right)
-        elif is_operand(right):
-            notation.append(left)
-            notation.append(right)
-            reduced_notation.appendleft(op)
-        else:
-            result = eval(int(left), int(right), op)
-            reduced_notation.appendleft(result)
-
-    reduced_notation.extendleft(notation)
-    return reduced_notation
-
-
 def is_operand(expr):
     return expr in ['+', '-', '/', '*']
 
@@ -50,14 +16,17 @@ def eval(left, right, op):
 
 
 def eval_reverse_polish_notation(notation):
-    while len(notation) > 2:
-        notation = reduce_notation(notation)
-        print(notation)
+    numbers = []
+    for expr in notation:
+        if is_operand(expr):
+            right = numbers.pop()
+            left = numbers.pop()
+            result = eval(left, right, expr)
+            numbers.append(result)
+        else:
+            numbers.append(int(expr))
 
-    if len(notation) > 1:
-        raise ValueError('Notation not reducable')
-
-    return notation[0]
+    return numbers.pop()
 
 
 eval_rpn = eval_reverse_polish_notation
