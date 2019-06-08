b7302c09dba1738da8d49bff9da4758ebc2b0f65
Lukas Martinelli <me@lukasmartinelli.ch>
2019-06-08 17:03:23 -0400

    Find better way for gcd

--- challenges/math/greatest_common_divisor.py
+++ challenges/math/greatest_common_divisor.py
@@ -1,13 +1,16 @@
 def greatest_common_divisor(m, n):
+    """
+    Using the Euclidean algorithm
+    https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
+    """
     low = min(m, n)
     high = max(m, n)
 
     if low == 0:
         return high
 
-    for divisor in reversed(xrange(1, low + 1)):
-        if low % divisor == 0 and high % divisor == 0:
-            return divisor
+    remainder = high % low
+    return greatest_common_divisor(low, remainder)
 
 
 def test_greatest_common_divisor():
@@ -19,3 +22,4 @@ def test_greatest_common_divisor():
     assert gcd(1, 1) == 1
     assert gcd(1, 0) == 1
     assert gcd(2, 0) == 2
+    assert gcd(270, 192) == 6
